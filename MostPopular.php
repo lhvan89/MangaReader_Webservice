7df0f596be37eeccd96ffc2da33efdd9ca6b04fb
vanlh <vanlh@msm.com.vn>
2020-09-07 15:29:25 +0700

    update

---
+++ MostPopular.php
@@ -0,0 +1,38 @@
+<?php include 'header.php'; ?>
+<?php include 'base_response.php'; ?>
+
+<?php
+
+$sql = <<<EOT
+SELECT m.id, m.title, m.url_image, m.status, c.title AS latest_chapter
+FROM chapter c INNER JOIN
+(SELECT MAX(id) id, manga_id
+FROM chapter
+GROUP BY manga_id) j ON c.id = j.id AND c.manga_id = j.manga_id
+INNER JOIN manga m ON m.id = c.manga_id
+EOT;
+
+$result = $conn->query($sql);
+
+$data = (array) null;
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $item = new stdClass();
+
+    $item->id       = $row["id"];
+    $item->title    = $row["title"];
+    $item->status   = $row["status"] == "Ongoing" ? $row["latest_chapter"] : $row["status"];
+    $item->thumnail = $row["url_image"];
+
+    $data[] = $item;
+    // if $item
+  }
+  $response->Result = $data;
+  echo json_encode($response);
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
