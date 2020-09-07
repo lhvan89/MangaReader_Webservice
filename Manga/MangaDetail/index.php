e46c68cf0cbfe080ce1ccc5b62815b4857d032ec
vanlh <vanlh@msm.com.vn>
2020-09-07 22:38:43 +0700

    update

--- Manga/MangaDetail/index.php
+++ Manga/MangaDetail/index.php
@@ -8,44 +8,84 @@ class Manga {
   public $title;
 }
 
+$id = $_GET['id'];
+
 // Get List Author
 $sql = <<<EOT
 SELECT a.name
 FROM author a, manga_author ma
-WHERE ma.author_id = a.id AND ma.manga_id = 1
+WHERE ma.author_id = a.id AND ma.manga_id = $id
+ORDER BY a.name  ASC
 EOT;
 $result = $conn->query($sql);
-$listAuthor = (array) null;
+$listAuthor = (array)null;
 if ($result->num_rows > 0) {
-  // output data of each row
   while($row = $result->fetch_assoc()) {
     $listAuthor[] = $row["name"];
   }
-  
 }
 
+// Get List Genere
+$sql = <<<EOT
+SELECT g.title
+FROM genre g, manga_genre mg
+WHERE mg.genre_id = g.id AND mg.manga_id = $id 
+ORDER BY g.title  ASC
+EOT;
+$result = $conn->query($sql);
+$listGenere = (array)null;
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $listGenere[] = $row["title"];
+  }
+}
+
+// Get List Chapter
+$sql = <<<EOT
+SELECT *
+FROM chapter c
+WHERE manga_id = $id
+ORDER BY title DESC
+EOT;
+$result = $conn->query($sql);
+$listChapter = (array)null;
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $item = new stdClass();
+    $item->id = (int)$row["id"];
+    $item->title = $row["title"];
+    $item->view = $row["view"];
+    $item->update_date = $row["update_date"];
+    $listChapter[] = $item;
+  }
+}
+
+//
 $sql = <<<EOT
 SELECT *
 FROM manga m
-WHERE m.status = "Completed"
+WHERE m.id = $id
 EOT;
 
 $result = $conn->query($sql);
 
-$data = (array) null;
+$data = new stdClass;
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     $item = new stdClass();
 
-    $item->id       = (int)$row["id"];
-    $item->title    = $row["title"];
-    $item->status   = $row["status"] == "Ongoing" ? $row["latest_chapter"] : $row["status"];
-    $item->thumbnail = $row["url_image"];
-    
+    $item->id         = (int)$row["id"];
+    $item->title      = $row["title"];
+    $item->status     = $row["status"];
+    $item->thumbnail  = $row["url_image"];
+    $item->authors    = implode(", ",$listAuthor);
+    $item->generes    = implode(" - ",$listGenere);
+    $item->description = $row["description"];
+    $item->chapters   = $listChapter;
+    $data = $item;
 
-    $data[] = $item;
   }
   $response->Result = $data;
   echo json_encode($response);
