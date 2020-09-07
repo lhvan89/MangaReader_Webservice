e1366d9fc65068428c8f1bf9b80086ed7471437a
vanlh <vanlh@msm.com.vn>
2020-09-07 14:09:28 +0700

    php connect server

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo("Hello")
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "MangaReader";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * From manga";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo $row["title"]; 
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
